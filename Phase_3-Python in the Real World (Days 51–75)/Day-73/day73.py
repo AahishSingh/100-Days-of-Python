cef0ae040888aef2dedb3b34cefd23e951691b5a
Aahish Aayan <aahishsingh2000@outlook.com>
2026-01-24 21:49:41 +0530

    Day 73: asyncio basics and async task execution

--- Phase_3-Python in the Real World (Days 51–75)/Day-73/day73.py
+++ Phase_3-Python in the Real World (Days 51–75)/Day-73/day73.py
@@ -14,6 +14,8 @@ async def main():
     start = time.time()
 
     await asyncio.gather(
+        # 2 seconds dealy added to each thread.
+        # Still the time taken to complete all the task would be almost 2 seconds, and not 6 seconds.
         simulate_io("Task 1", 2),
         simulate_io("Task 2", 2),
         simulate_io("Task 3", 2),
