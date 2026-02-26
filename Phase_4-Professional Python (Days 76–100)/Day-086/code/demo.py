10bca167b1ed8739b068e87aeebf0d471a754880
Silver Bullet <aahishsingh2000@outlook.com>
2026-02-26 17:55:23 +0530

    Day 86: Virtual Environments & Isolation

---
+++ Phase_4-Professional Python (Days 76–100)/Day-086/code/demo.py
@@ -0,0 +1,16 @@
+# Day 86
+
+
+import sys
+
+
+def main():
+    print("Python executable:")
+    print(sys.executable)
+
+    print("\nPython version:")
+    print(sys.version)
+
+
+if __name__ == "__main__":
+    main()
